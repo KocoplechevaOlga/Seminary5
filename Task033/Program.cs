69d57f3fa2fa09bdb6f60cea729b8db31d3fff89
Olga Kosoplecheva <olgakosoplecheva@gmail.ru>
2022-11-15 18:40:08 +0300

    Task 33

--- Task033/Program.cs
+++ Task033/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+//Например: 4; массив [6, 7, 19, 345, 3] -> нет; 3; массив [6, 7, 19, 345, 3] -> да 1n
+
+int[] FillArray(int l, int min, int max)
+{
+    int[] array = new int[l];
+    Random rnd  = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max);
+    } 
+    return array;
+}
+void PrintArray(int[] arr)
+{
+    int j = 0;
+    Console.Write("[");
+    for (j = 0; j < (arr.Length-1); j++)
+    {
+        Console.Write(arr[j] + ", ");
+    }
+    Console.Write(arr[j]);
+    Console.Write("]");
+}
+bool FindDig(int [] arr, int n)
+{
+    bool b = false;
+    for (int i = 0; i < (arr.Length); i++)
+    {
+        if (arr[i] == n) 
+        {
+            b = true;
+            break;
+        }
+    }    
+    return b;
+}
+
+Console.WriteLine("Введите размер массива:");
+int length = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальное ограничение массива:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное ограничение массива:");
+int b = Convert.ToInt32(Console.ReadLine())+1;
+int [] myArray = FillArray(length, a, b);
+PrintArray(myArray);
+Console.WriteLine(" ");
+Console.WriteLine("Введите искомое число:");
+int l = Convert.ToInt32(Console.ReadLine());
+
+bool find = FindDig (myArray, l);
+Console.WriteLine (FindDig (myArray, l) ? " ->  Да" : " ->  Нет");
